45f0caebdb90faf5cf34747e5029d4f0ac372c50
Felipe Tagawa <felipereisbkp@gmail.com>
2026-09-15 09:49:24 -0300

    feat: treino e predição feitos

--- aula5.1/exGuiado.py
+++ aula5.1/exGuiado.py
@@ -1,7 +1,39 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import pandas as pd
 
 iris = load_iris()
 X = iris.data
 y = iris.target
 
-print(X.shape, y.shape)
+# print(X.shape, y.shape)
+
+df = pd.DataFrame(X, columns=iris.feature_names)
+df ['especie'] = iris.target_names[y]
+
+print(df.groupby('especie').mean().round(2))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4,
+    stratify=y, random_state=42
+)
+
+#print(X_train.shape, X_test.shape)
+
+model = KNeighborsClassifier(
+    n_neighbors=5,
+)
+
+# Treinamento
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(y_pred[:8])
+print(y_test[:8])
+
+print(accuracy_score(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
